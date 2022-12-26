e94c913564651cc8830a513e3b0cc01fffe7614c
Susmita-Dey <susmitadey475@gmail.com>
2022-12-27 01:17:46 +0530

    Add deployed server link to frontend

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server -> bot's response
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codezilla.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
