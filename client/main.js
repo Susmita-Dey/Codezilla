3a2419b5a03012bda910f5ca76226d0ef94f5a3a
cu8code <git8ankanroy@gmail.com>
2022-12-28 10:53:38 +0530

    added some addEventListner

--- client/main.js
+++ client/main.js
@@ -1,17 +1,18 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
+document.querySelector("#mainNav .openBtn").addEventListener("click", () => {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("mainNav").style.marginLeft = "250px";
-}
+})
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
+document.querySelector("#mySidebar .closeBtn").addEventListener("click", () => {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("mainNav").style.marginLeft = "0";
-}
+})
 
 // Toggle mode
 function toggleMode() {
+  console.log("lol")
   var element = document.body;
   var eleApp = document.getElementById("#app");
   element.classList.toggle("light-mode");
